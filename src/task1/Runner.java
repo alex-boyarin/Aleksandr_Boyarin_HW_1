a79e19a3e8bba571c3d8edf538a78450ef418fbe
Aleksandr Boyarin <a.boyarin86@gmail.com>
2021-11-10 17:26:24 +0300

    First task completed.

--- src/task1/Runner.java
+++ src/task1/Runner.java
@@ -1,6 +1,7 @@
 package task1;
 
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 import static task1.DayOfTheWeek.*;
 
@@ -10,20 +11,24 @@ public class Runner {
         System.out.println("Введите число от 1 до 7\n" +
                 "Для выхода нажмите 0");
         while (true) {
-            int numberOfTheDay = scanner.nextInt();
-            if (numberOfTheDay == 0) {
+            String numberOfTheDay = scanner.next();
+            if (numberOfTheDay.equals("0")) {
                 break;
             }
-            switch (numberOfTheDay) {
-                case 1 -> System.out.println(MONDAY);
-                case 2 -> System.out.println(TUESDAY);
-                case 3 -> System.out.println(WEDNESDAY);
-                case 4 -> System.out.println(THURSDAY);
-                case 5 -> System.out.println(FRIDAY);
-                case 6 -> System.out.println(SATURDAY);
-                case 7 -> System.out.println(SUNDAY);
-                default -> System.out.println("Не верный ввод!!!\n" +
+            if (Pattern.matches("[1-7]", numberOfTheDay)) {
+                switch (Integer.parseInt(numberOfTheDay)) {
+                    case 1 -> System.out.println(MONDAY);
+                    case 2 -> System.out.println(TUESDAY);
+                    case 3 -> System.out.println(WEDNESDAY);
+                    case 4 -> System.out.println(THURSDAY);
+                    case 5 -> System.out.println(FRIDAY);
+                    case 6 -> System.out.println(SATURDAY);
+                    case 7 -> System.out.println(SUNDAY);
+                }
+            } else {
+                System.out.println("Не верный ввод!!!\n" +
                         "Число должно быть от 1 до 7");
+
             }
         }
     }
